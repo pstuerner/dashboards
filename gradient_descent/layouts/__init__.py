93f56b949230149dbd10c9d408febca133653efd
Philipp Stuerner <philipp.stuerner@web.de>
2021-03-23 18:48:49 +0000

    implement layout changes

--- gradient_descent/layouts/__init__.py
+++ gradient_descent/layouts/__init__.py
@@ -1,4 +1,6 @@
-from .example1 import layout as example1_layout
+from .section1 import section1_layout
 from .example2 import layout as example2_layout
 from .example3 import layout as example3_layout
 from .example4 import layout as example4_layout
+from .toc import toc
+from .introduction import introduction
